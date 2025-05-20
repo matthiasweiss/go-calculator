d8afb17bf5848ad51ec16c5cbe6ad7c4b88900dd
Matthias Weiß <mail@matthiasweiss.at>
2025-05-20 20:49:29 +0200

    go routine example

---
+++ calculator-concurrent/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := [][]int{
+		{0, 1, 2, 3},
+		{4, 5, 6, 7},
+	}
+
+	c := make(chan int)
+
+	for _, v := range numbers {
+		go sum(c, v)
+	}
+
+	sums := []int{}
+
+	for range numbers {
+		sums = append(sums, <-c)
+	}
+
+	fmt.Println(sums)
+}
+
+func sum(c chan int, numbers []int) {
+	sum := 0
+
+	for _, v := range numbers {
+		sum += v
+	}
+
+	c <- sum
+}
